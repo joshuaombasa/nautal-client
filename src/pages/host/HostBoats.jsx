5ab1e1e88527de5c96f876be8b2e9e686bc33b82
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-15 17:14:38 +0300

    added suspence to display a fallback until children components have finished loading

--- src/pages/host/HostBoats.jsx
+++ src/pages/host/HostBoats.jsx
@@ -1,28 +1,41 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { Suspense } from "react";
+import { Await, defer, useLoaderData } from "react-router-dom";
 import { getHostBoats } from "../../api";
 import HostBoatItem from "../../components/HostBoatItem";
 import { authRequired } from "../../utils";
 
-export async function loader({request}) {
+export async function loader({ request }) {
     await authRequired(request)
-    return getHostBoats()
+    const hostBoatsPromise = getHostBoats()
+    return defer({ hostBoats: hostBoatsPromise })
 }
 
 export default function HostBoats() {
-    const hostBoats = useLoaderData()
+    const dataPromise = useLoaderData()
 
-    const hostBoatElements = hostBoats.map(boat => (
-        <HostBoatItem key={boat.id} boat={boat}/>
-    ))
-    
 
-    return (
-        <div className="host--boats--page">
+
+    function rendeHostBoats(hostBoats) {
+
+        const hostBoatElements = hostBoats.map(boat => (
+            <HostBoatItem key={boat.id} boat={boat} />
+        ))
+
+        return (
             <div className="host--boats-container">
                 <h1>Your listed boats</h1>
                 {hostBoatElements}
             </div>
+        )
+    }
+
+    return (
+        <div className="host--boats--page">
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.hostBoats}>
+                    {rendeHostBoats}
+                </Await>
+            </Suspense>
         </div>
     )
 }
