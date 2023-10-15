77cc143ca7d078a16ac5ba791a9f1d38fa0db698
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-10-15 17:14:21 +0300

    added suspence to display a fallback until children components have finished loading

--- src/pages/boats/SingleBoat.jsx
+++ src/pages/boats/SingleBoat.jsx
@@ -1,27 +1,28 @@
-import React from "react";
+import React, { Suspense } from "react";
 
-import { Link, useLoaderData, useLocation } from "react-router-dom";
+import { Await, defer, Link, useLoaderData, useLocation } from "react-router-dom";
 import { getSigleBoat } from "../../api";
 
-export function loader({params, request}) {
-    return getSigleBoat(params.id)
+export function loader({ params, request }) {
+    const boatPromise = getSigleBoat(params.id)
+    return defer({ boat: boatPromise })
 }
 
 export default function Singleboat() {
-    const data = useLoaderData()
-    const SingleBoat = data[0]
+    const dataPromise = useLoaderData()
 
     const location = useLocation()
     const searchParams = location.state.searchParams ? `?${location.state.searchParams}` : ""
-    const typeFilter  = location.state.typeFilter? location.state.typeFilter : "all"
+    const typeFilter = location.state.typeFilter ? location.state.typeFilter : "all"
 
-    return (
-        <div className="single--boat--page">
+    function renderBoat(boat) {
+        const SingleBoat = boat[0]
+        return (
             <div className="single--boat--page--container">
-                <Link 
-                     to={`..${searchParams}`} 
-                     relative="path"
-                     className="back--link"
+                <Link
+                    to={`..${searchParams}`}
+                    relative="path"
+                    className="back--link"
                 >&larr; <span>Back to {typeFilter} boats</span></Link>
                 <img src={SingleBoat.imageUrl} alt="" />
                 <span className="boat--type">{SingleBoat.type}</span>
@@ -30,6 +31,16 @@ export default function Singleboat() {
                 <p>{SingleBoat.description}</p>
                 <Link className="gold--link--style">Rent this van</Link>
             </div>
+        )
+    }
+
+    return (
+        <div className="single--boat--page">
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Await resolve={dataPromise.boat}>
+                    {renderBoat}
+                </Await>
+            </Suspense>
         </div>
     )
 }
