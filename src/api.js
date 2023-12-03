ec77d761d32c6062c1138ec9eae9fea827a8524f
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2023-12-03 23:08:48 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,15 +1,5 @@
 export async function getBoats() {
-    const res = await fetch("http://localhost:3000/api/yatchs")
-    const data = await res.json()
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            status: res.status,
-            statusText : res.statusText
-        }
-    }
-
-    return data
+    
 }
 
 export async function getSigleBoat(id) {
