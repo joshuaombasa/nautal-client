5c8d88e6a9a55b78665cca2fd17cb20fd58b60ed
Joshua Ombasa <114947718+joshuaombasa@users.noreply.github.com>
2023-12-03 23:08:01 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,10 @@
 import { redirect } from "react-router-dom";
 
 export async function authRequired(request) {
-    
+    const url = new URL(request.url)
+    const pathname = url.pathname ? url.pathname : "/host"
+    const isLoggedIn = localStorage.getItem("isLoggedIn")
+    if (!isLoggedIn) {
+        throw redirect(`/login?message=Please login first&redirectPathname=${pathname}`)
+    }
 }
